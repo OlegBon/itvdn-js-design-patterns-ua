0d58afe36c4684de90937e0f6a82c0c0b9f467e0
OlegBon <oleh.bon@yahoo.com>
2025-07-31 11:54:23 +0300

    add tasks 01 for lesson 001

---
+++ 001/001-script.js
@@ -0,0 +1,13 @@
+// Створіть два об'єкти за допомогою літералу об’єкта та перевірте їх на рівність.
+
+const object1 = {
+  name: "Object One",
+  age: 25,
+};
+
+const object2 = {
+  name: "Object One",
+  age: 25,
+};
+
+console.log("object1 === object2", object1 === object2);
